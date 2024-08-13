e3b5d5c29f0bd4fdf169d6cd2c3d35fa8e68e940
YuriQuest <yurimarques.el@gmail.com>
2024-08-13 20:31:57 -0300

    :recycle: refactor: criação de informações basicas

--- scripts/managers/basic.gd
+++ scripts/managers/basic.gd
@@ -1,1 +1,32 @@
+@tool
+extends Node
+class_name GlobalVelocity
 
+var _windowSize =  DisplayServer.screen_get_size()
+
+var _speed: float
+var _accel: float
+var _buff:  float
+
+func get_speed():
+	return _speed
+
+func set_speed():
+	#var floatWindowSize = _windowSize * 1.0
+	var _calculation: float = _windowSize.x + (_buff * 10)
+	print(_calculation)
+	_speed = _calculation
+	
+func get_accel():
+	return _accel
+
+func set_accel():
+	
+	var _calculation: float = + (_buff * 10)
+	_accel = _calculation
+
+func get_buff():
+	return _buff
+
+func set_buff(buff: int):
+	_buff = buff
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,13 +1,17 @@
 extends CharacterBody2D
 
-@export var speed: float = 240
-@export var accel: float = 24
+var player = GlobalVelocity.new()
+
+func _init():
+	player.set_buff(0)
+	player.set_speed()
+	player.set_accel()
 
 func _physics_process(_delta:float) -> void:
 	var direction: Vector2 = Input.get_vector("left","right", "up", "down")
 
-	velocity.x = move_toward(velocity.x, speed * direction.x, accel)
-	velocity.y = move_toward(velocity.y, speed * direction.y, accel)
+	velocity.x = move_toward(velocity.x, player.get_speed() * direction.x, player.get_accel())
+	velocity.y = move_toward(velocity.y, player.get_speed() * direction.y, player.get_accel())
 
 	move_and_slide()
 
